2f297ff9b6ab4bd8dca5e5ebfae8b1d1d96bc3f7
Imran <imran.ali@iqvia.com>
2022-11-15 16:09:27 +0600

    demo

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// counter up js start
+$('.counter').counterUp({
+    delay: 10,
+    time: 1000
+});
+// counter up js end
+
+// slick slider start
+$('.one').slick({
+    
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow:".next",
+    prevArrow:".prev",
+    infinite: true,
+    autoplay:true,
+    
+  });
+  // slick slider end
+
+  
